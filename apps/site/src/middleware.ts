02199e371aeca66d5d93194cd6d852e3d166b2bd
SahilMahadwar <accounts@mahadwar.com>
2023-03-13 22:11:54 +0530

    chore removed console logs

--- apps/site/src/middleware.ts
+++ apps/site/src/middleware.ts
@@ -13,29 +13,29 @@ export const config = {
   ],
 };
 
-console.log("Rewrites");
-
 export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
-
-  console.log("Rewrites");
 
   // Get hostname of request (e.g. demo.portify.site, demo.localhost:3000)
   const hostname = req.headers.get("host") || "demo.portify.site";
 
-  console.log("Hostname" + hostname);
+  console.log("Hostname " + hostname);
 
   // Get the pathname of the request (e.g. /, /about, /blog/first-post)
   const path = url.pathname;
 
-  console.log("path" + path);
+  console.log("path " + path);
 
   const currentHost =
     process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
       ? hostname.replace(`.portify.site`, "")
-      : hostname.replace(`.localhost:3000`, "");
+      : hostname.replace(`.localhost:3001`, "");
 
-  console.log("currentHost" + currentHost);
+  console.log("currentHost " + currentHost);
+
+  console.log(
+    NextResponse.rewrite(new URL(`/_sites/${currentHost}${path}`, req.url))
+  );
 
   // rewrite everything else to `/_sites/[site] dynamic route
   return NextResponse.rewrite(
