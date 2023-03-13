759c808c81e385c11296772f1739a989d746ae10
SahilMahadwar <accounts@mahadwar.com>
2023-03-13 22:31:15 +0530

    add: tailwind and fonts

--- apps/app/src/pages/_app.tsx
+++ apps/app/src/pages/_app.tsx
@@ -4,8 +4,19 @@ import { api } from "../utils/api";
 
 import "../styles/globals.css";
 
+import { Inter, Lexend_Deca } from "next/font/google";
+
+const lexend = Lexend_Deca({
+  subsets: ["latin"],
+  variable: "--font-lexend",
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${lexend.variable}  `}>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default api.withTRPC(MyApp);
---
+++ apps/app/tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require("tailwindcss/colors");
+const { join } = require("path");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [join(__dirname, "./src/**/*.{js,ts,jsx,tsx}")],
+  theme: {
+    extend: {
+      colors: {
+        brand: colors.emerald,
+      },
+    },
+    fontFamily: {
+      display: ["var(--font-lexend), -apple-system, system-ui, sans-serif"],
+      body: ["var(--font-lexend), -apple-system, system-ui, sans-serif"],
+    },
+  },
+};
